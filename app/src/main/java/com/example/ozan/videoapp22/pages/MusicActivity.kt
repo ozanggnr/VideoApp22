76b6abca586bf93b69ed1536e716a203492315cd
Max1m1m1 <ozan2000gngr@hotmail.com>
2024-08-12 09:19:53 +0300

    Transfer into complete Service

--- app/src/main/java/com/example/ozan/videoapp22/pages/MusicActivity.kt
+++ app/src/main/java/com/example/ozan/videoapp22/pages/MusicActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
